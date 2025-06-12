b81a974a80d86917a33c40376aa946384efd3b97
sahilsawantt <sawantsahil591@gmail.com>
2025-06-12 16:41:39 +0530

    s

--- src/components/IllegalActivity.js
+++ src/components/IllegalActivity.js
@@ -1,11 +1,23 @@
-import React from 'react';
+// import React from 'react';
+
+// function IllegalActivity() {
+//   return (
+//     <div className="feature-card">
+//       <img src="./images/illegalactivity.png" alt="Illegal Activity" />
+//       <h3>Illegal Activities</h3>
+//       <p>Let us know if something illegal is happening around you.</p>
+//     </div>
+//   );
+// }
+
+// export default IllegalActivity;
 
 function IllegalActivity() {
   return (
-    <div className="feature-card">
-      <img src="./images/illegalactivity.png" alt="Illegal Activity" />
+    <div className="feature-box">
+      <img src="/images/illegalactivity.png" alt="Illegal Activity" style={{ width: '100%', height: '200px', objectFit: 'cover' }} />
       <h3>Illegal Activities</h3>
-      <p>Let us know if something illegal is happening around you.</p>
+      <p>Report any suspicious or illegal actions happening nearby.</p>
     </div>
   );
 }
