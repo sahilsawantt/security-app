ccb5490ef80fd955901df75944a740a09fb6beb0
sahilsawantt <sawantsahil591@gmail.com>
2025-06-12 16:42:44 +0530

    s

--- src/components/IllegalActivity.js
+++ src/components/IllegalActivity.js
@@ -1,25 +1,25 @@
-// import React from 'react';
-
-// function IllegalActivity() {
-//   return (
-//     <div className="feature-card">
-//       <img src="./images/illegalactivity.png" alt="Illegal Activity" />
-//       <h3>Illegal Activities</h3>
-//       <p>Let us know if something illegal is happening around you.</p>
-//     </div>
-//   );
-// }
-
-// export default IllegalActivity;
+import React from 'react';
 
 function IllegalActivity() {
   return (
-    <div className="feature-box">
-      <img src="/images/illegalactivity.png" alt="Illegal Activity"  />
+    <div className="feature-card">
+      <img src="./images/illegalactivity.png" alt="Illegal Activity" />
       <h3>Illegal Activities</h3>
-      <p>Report any suspicious or illegal actions happening nearby.</p>
+      <p>Let us know if something illegal is happening around you.</p>
     </div>
   );
 }
 
 export default IllegalActivity;
+
+// function IllegalActivity() {
+//   return (
+//     <div className="feature-box">
+//       <img src="/images/illegalactivity.png" alt="Illegal Activity"  />
+//       <h3>Illegal Activities</h3>
+//       <p>Report any suspicious or illegal actions happening nearby.</p>
+//     </div>
+//   );
+// }
+
+// export default IllegalActivity;
