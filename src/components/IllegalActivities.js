82a78836ec2cc3d9750fa580f1102d5f42fbfc8c
sahilsawantt <sawantsahil591@gmail.com>
2025-06-13 10:11:16 +0530

    s

---
+++ src/components/IllegalActivities.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import illegalactivityImg from '../images/illegalactivity.png'; // image add karna hoga
+
+function IllegalActivities() {
+  return (
+    <div className="feature-card">
+      <img src={illegalactivitylImg} alt="Illegal Activities" />
+      <h3>Illegal Activities</h3>
+      <p>Report activities like drug dealing, trafficking, or other illegal incidents.</p>
+    </div>
+  );
+}
+
+export default IllegalActivities;
