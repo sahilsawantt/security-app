5c228543cf6852550537e30f88216f0350f599c0
sahilsawantt <sawantsahil591@gmail.com>
2025-06-13 10:30:48 +0530

    s

--- src/components/ladydriver.js
+++ src/components/ladydriver.js
@@ -4,7 +4,7 @@ import ladydriver from '../images/ladydriver.png';
 function ladydriver() {
   return (
     <div className="feature-card">
-      <img src={LadyDriverImg} alt="Lady Driver" />
+      <img src={ladydriverImg} alt="Lady Driver" />
       <h3>Illegal Activities</h3>
       <p>Report any kind of illegal activities happening in your area anonymously.</p>
     </div>
