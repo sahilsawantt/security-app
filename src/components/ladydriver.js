8f1b54d835c3cd7b5e21b46019ebe85d9da55242
sahilsawantt <sawantsahil591@gmail.com>
2025-06-13 10:26:50 +0530

    S

--- src/components/ladydriver.js
+++ src/components/ladydriver.js
@@ -4,7 +4,7 @@ import driverImg from '../images/ladydriver.png';
 function LadyDriver() {
   return (
     <div className="feature-box">
-      <img src={driverImg} alt="Lady Driver" style={{ width: '100%', height: 'auto', borderRadius: '10px' }} />
+      <img src={driverImg} alt="Lady Driver" />
       <h3>Lady Driver Cab</h3>
       <p>Night-time safe cab booking with verified lady drivers for secure travel.</p>
     </div>
