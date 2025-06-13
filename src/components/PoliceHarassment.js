a9579fde0384f7f1a4c60bf5943fbd709ac9f262
sahilsawantt <sawantsahil591@gmail.com>
2025-06-13 09:48:58 +0530

    s

--- src/components/PoliceHarassment.js
+++ src/components/PoliceHarassment.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import policeImg from '../images/PoliceHarassment.png'; 
 
 function PoliceHarassment() {
   return (
     <div className="feature-card">
-      <img src="./images/policeharassment.png" alt="Police Harassment" />
+      <img src={policeImg} alt="Police Harassment" />
       <h3>Police Harassment</h3>
-      <p>Report any kind of misconduct or harassment by authorities.</p>
+      <p>Report any kind of harassment or misconduct by police officers.</p>
     </div>
   );
 }
