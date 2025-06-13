9fdc24c8c8c07173907fc57e2438712d1d818d0c
sahilsawantt <sawantsahil591@gmail.com>
2025-06-13 11:25:31 +0530

    s

--- src/components/privacydelete.js
+++ src/components/privacydelete.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import privacyImg from '../images/privacydelete.png'; // इमेज का नाम और path सही रखें
-
-function PrivacyDelete() {
-  return (
-    <div className="feature-card">
-      <img src={privacyImg} alt="Privacy Delete" />
-      <h3>Privacy Delete</h3>
-      <p>Get immediate help in deleting private or sensitive photos/videos from the internet or device.</p>
-    </div>
-  );
-}
-
-export default PrivacyDelete;
-
 // import React from 'react';
-// import { Link } from 'react-router-dom';
-// import privacyImg from '../images/privacydelete.png';
+// import privacyImg from '../images/privacydelete.png'; // इमेज का नाम और path सही रखें
 
 // function PrivacyDelete() {
 //   return (
-//     <Link to="/privacydelete" className="feature-card">
+//     <div className="feature-card">
 //       <img src={privacyImg} alt="Privacy Delete" />
 //       <h3>Privacy Delete</h3>
-//       <p>Get help deleting private or sensitive media instantly.</p>
-//     </Link>
+//       <p>Get immediate help in deleting private or sensitive photos/videos from the internet or device.</p>
+//     </div>
 //   );
 // }
 
 // export default PrivacyDelete;
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import privacyImg from '../images/privacydelete.png';
+
+function PrivacyDelete() {
+  return (
+    <Link to="/privacydelete" className="feature-card">
+      <img src={privacyImg} alt="Privacy Delete" />
+      <h3>Privacy Delete</h3>
+      <p>Get help deleting private or sensitive media instantly.</p>
+    </Link>
+  );
+}
+
+export default PrivacyDelete;
