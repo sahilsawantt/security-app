601a87671e6239b1b487d0f7010276c009242706
sahilsawantt <sawantsahil591@gmail.com>
2025-06-13 10:18:39 +0530

    s

--- src/components/IllegalActivities.js
+++ src/components/IllegalActivities.js
@@ -4,7 +4,7 @@ import illegalactivities from '../images/illegalactivities.png';
 function IllegalActivities() {
   return (
     <div className="feature-card">
-      <img src={IllegalActivitiesImg} alt="Illegal Activities" />
+      <img src={illegalactivitiesImg} alt="illegalactivities" />
       <h3>Illegal Activities</h3>
       <p>Report activities like drug dealing, trafficking, or other illegal incidents.</p>
     </div>
