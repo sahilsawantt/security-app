d9c50b9ed52d688153ccf6afbd4be7801647a142
sahilsawantt <sawantsahil591@gmail.com>
2025-06-13 10:45:50 +0530

    s

--- src/components/ladydriver.js
+++ src/components/ladydriver.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ladydriver from '../images/ladydriver.png';
+import ladyDriverImg from '../images/ladydriver.png'; 
 
 function LadyDriver() {
   return (
     <div className="feature-card">
-      <img src={ladydriverImg} alt="Lady Driver" />
-      <h3>Illegal Activities</h3>
-      <p>Report any kind of illegal activities happening in your area anonymously.</p>
+      <img src={ladyDriverImg} alt="Lady Driver" />
+      <h3>Lady Driver</h3>
+      <p>Book a trusted lady driver for safe travel at night or during emergencies.</p>
     </div>
   );
 }
