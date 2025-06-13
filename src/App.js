17d51dd3aa3868ce05152fff65667794b39c81b8
sahilsawantt <sawantsahil591@gmail.com>
2025-06-13 11:20:32 +0530

    s

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react'; import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; import './App.css';
 
-// Component Cards import LadyDriver from './components/ladydriver'; import LegalSupport from './components/legalsupport'; import PrivacyDelete from './components/PrivacyDelete'; import Emergency from './components/emergencyhelp'; import HarassmentReport from './components/harassment'; import StreetViolence from './components/street_violence'; import HospitalHelp from './components/hospitalhelp'; import PoliceHarassment from './components/PoliceHarassment'; import UnsafeZone from './components/UnsafeZone'; import IllegalActivities from './components/IllegalActivities';
-
-// Pages import LadyDriverPage from './pages/LadyDriverPage'; import LegalSupportPage from './pages/LegalSupportPage'; import PrivacyDeletePage from './pages/PrivacyDeletePage'; import EmergencyPage from './pages/EmergencyPage'; import HarassmentReportPage from './pages/HarassmentReportPage'; import StreetViolencePage from './pages/StreetViolencePage'; import HospitalHelpPage from './pages/HospitalHelpPage'; import PoliceHarassmentPage from './pages/PoliceHarassmentPage'; import UnsafeZonePage from './pages/UnsafeZonePage'; import IllegalActivitiesPage from './pages/IllegalActivitiesPage';
+import LadyDriver from './components/ladydriver';
+import LegalSupport from './components/legalsupport';
+import PrivacyDelete from './components/PrivacyDelete';
+import Emergency from './components/emergancyhelp';
+import HarassmentReport from './components/harassment';
+import StreetViolence from './components/street_violence';
+import HospitalHelp from './components/hospitalhelp';
+import PoliceHarassment from './components/PoliceHarassment';
+import UnsafeZone from './components/UnsafeZone';
+import IllegalActivities from './components/IllegalActivities';
 
 function App() { return ( <Router> <div className="app"> <header className="header"> <div className="branding">Guarantee Helper</div> <input type="text" placeholder="Search..." className="search-box" /> </header>
 
