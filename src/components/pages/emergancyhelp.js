02dfd37683f5f68639b76df8eef6c15c7c4b8a08
sahilsawantt <sawantsahil591@gmail.com>
2025-06-14 17:36:31 +0530

    s

---
+++ src/components/pages/emergancyhelp.js
@@ -0,0 +1,13 @@
+// src/pages/EmergencyHelp.js
+import React from "react";
+
+function EmergencyHelp() {
+  return (
+    <div>
+      <h1>Emergency Help</h1>
+      <p>This page is for emergency support.</p>
+    </div>
+  );
+}
+
+export default EmergencyHelp;
